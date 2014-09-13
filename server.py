4f5d8cd4f6d30fdf0918e56c8ec1527f8f583878
Patrick Xia <xiapatrick418@hotmail.com>
2014-09-13 02:15:07 -0400

    orientation part 1

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, g, request
 from flask_cake import Cake
+import math
+
 app = Flask(__name__)
 cake = Cake(app)
 
 path = []
+(headingx, headingy) = (0.0, 1.0) 
+prevInstr = (0, 0, 0)
 @app.route('/')
 def index():
     del path[:]
+    
     return render_template('index.html')
 
 class Point:
@@ -27,6 +32,7 @@ def update():
     delta = int(request.args.get('delta'))
     color = (int(request.args.get('r')), int(request.args.get('g')), int(request.args.get('b')))
     start = request.args.get('start')
+    #heading = request.args.get('heading') method does not exist yet.
     path.append(Point(x, y, delta, start, color))
 
     return ''
@@ -34,9 +40,30 @@ def update():
 # returns (left motor energy, right motor energy, duration, light on, light r, light g, light b)
 @app.route('/query')
 def query():
-    args = [1, 0, 0, 0, 0, 0, 0]
+    print path
+    if len(path) < 2:
+        return ' '.join(map(repr, [0,0,0,0,0,0,0]))
+    (currpt, prevpt) = (path[0], path[0])
+    (L, R, t) = reorient((currpt.x, currpt.y), (prevpt.x, prevpt.y))
+    #fix thing but we ignore this for now.
+    args = [L, R, t, 4, 3, 2, 1]
     return ' '.join(map(repr, args))
 
+#returns the energy levels needed to reorient from (a,b) to (c,d)
+def reorient((a,b), (c,d)):
+    quarter = math.pi/4
+    quarterTime = 4
+    (x,y) = (c-a, d-b)
+    magnitude = math.hypot(x,y)
+    (unitx, unity) = (x / magnitude, y / magnitude)
+    turnAngle = math.acos(unitx*headingx + unity*headingy)
+    turnDir = 1 if turnAngle < quarter else -1
+    #naive for now
+    return (100 * turnDir, -100 * turnDir, quarterTime * (turnAngle / quarter))
+
+    
+
+#return a set of directions to be run 
 if __name__ == "__main__":
     cake.init_app(app)
     app.run(host='0.0.0.0', debug=True)
